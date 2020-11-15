52d27066aad697e884608fd63626c4e5e68f7183
Marek Kobida <marek.kobida@gmail.com>
2020-11-15 19:55:10 +0100

    updated bin

--- swift.js
+++ swift.js
@@ -10,6 +10,22 @@ const { applicationsToCompile } = require(path.resolve(
   './package.json',
 ));
 
-webpack(applications(applicationsToCompile), (error, $) =>
-  console.log($.toString({ colors: true })),
-);
+const compiler = webpack(applications(applicationsToCompile));
+
+compiler.watch({}, (error, test) => {
+  const json = test.toJson();
+
+  if (test.hasErrors()) {
+    json.errors.forEach((error, i) =>
+      console.log(`[${i + 1}] \x1b[31m${error.message}\x1b[0m`),
+    );
+  }
+
+  json.children.forEach((child, i) =>
+    child.assets.forEach((asset, ii) =>
+      console.log(
+        `[${i + 1}][${ii + 1}] ${path.resolve(child.outputPath, asset.name)}`,
+      ),
+    ),
+  );
+});
