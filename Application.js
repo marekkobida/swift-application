8452269b5554f2a3365e7a5b4dfa262fb4acb42b
Marek Kobida <marek.kobida@gmail.com>
2020-11-16 18:44:46 +0100

    updated

--- Application.js
+++ Application.js
@@ -2,87 +2,23 @@
 /*
  * Copyright 2020 Marek Kobida
  */
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const http_1 = __importDefault(require("http"));
-const path_1 = __importDefault(require("path"));
 class Application {
-    constructor(description, name, version) {
+    constructor(description, htmlFileUrl, name, version) {
         this.description = description;
+        this.htmlFileUrl = htmlFileUrl;
         this.name = name;
         this.version = version;
-        this.httpServerSockets = new Set();
-        this.httpServer = this.createHttpServer();
-        /* ---------------------------------------------------------------- */
-        process.on('message', (serverIPCMessage) => {
-            if (serverIPCMessage.name === 'AFTER_ADD') {
-                this.afterAdd();
-            }
-            if (serverIPCMessage.name === 'DELETE') {
-                this.sendIPCMessage({
-                    application: this.toJSON(),
-                    name: 'AFTER_DELETE',
-                });
-                /* ---------------------------------------------------------------- */
-                this.httpServer.close();
-                /* ---------------------------------------------------------------- */
-                this.httpServerSockets.forEach(socket => {
-                    socket.destroy();
-                    this.httpServerSockets.delete(socket);
-                });
-                /* ---------------------------------------------------------------- */
-                this.afterDelete();
-            }
-        });
-        /* ---------------------------------------------------------------- */
-        this.sendIPCMessage({
-            application: this.toJSON(),
-            name: 'ADD',
-        });
     }
     afterAdd() { }
     afterDelete() { }
-    createHttpServer() {
-        const httpServer = http_1.default.createServer((request, response) => {
-            if (request.url === '/about') {
-                response.setHeader('Content-Type', 'application/json');
-                response.end(JSON.stringify(this.toJSON()));
-            }
-        });
-        /* ---------------------------------------------------------------- */
-        httpServer.on('connection', socket => {
-            this.httpServerSockets.add(socket);
-            httpServer.once('close', () => this.httpServerSockets.delete(socket));
-        });
-        /* ---------------------------------------------------------------- */
-        httpServer.listen();
-        /* ---------------------------------------------------------------- */
-        return httpServer;
-    }
-    httpServerUrl() {
-        const $ = this.httpServer.address();
-        return $ !== null && typeof $ === 'object'
-            ? `http://127.0.0.1:${$.port}`
-            : '';
-    }
-    sendIPCMessage(clientIPCMessage) {
-        process.send?.(clientIPCMessage);
-    }
     toJSON() {
         return {
             description: this.description,
-            htmlFileUrl: this.updateHtmlFileUrl(),
-            httpServerUrl: this.httpServerUrl(),
+            htmlFileUrl: this.htmlFileUrl,
             name: this.name,
             version: this.version,
         };
-    }
-    updateHtmlFileUrl() {
-        const htmlFileUrl = new URL(`file://${path_1.default.resolve(__dirname, './client.html')}`);
-        htmlFileUrl.searchParams.set('httpServerUrl', this.httpServerUrl());
-        return htmlFileUrl.toString();
     }
 }
 exports.default = Application;
---
+++ NativeApplication.d.ts
@@ -0,0 +1,33 @@
+/// <reference types="node" />
+import http from 'http';
+import net from 'net';
+export interface ClientIPCMessage {
+    application: ReturnType<NativeApplication['toJSON']>;
+    name: 'ADD' | 'AFTER_DELETE';
+}
+export interface ServerIPCMessage {
+    name: 'AFTER_ADD' | 'DELETE';
+}
+declare class NativeApplication {
+    readonly description: string;
+    readonly htmlFileUrl: string;
+    readonly name: string;
+    readonly version: string;
+    httpServer: http.Server;
+    httpServerSockets: Set<net.Socket>;
+    constructor(description: string, htmlFileUrl: string, name: string, version: string);
+    afterAdd(): void;
+    afterDelete(): void;
+    private createHttpServer;
+    private httpServerUrl;
+    sendIPCMessage(clientIPCMessage: ClientIPCMessage): void;
+    toJSON(): {
+        description: string;
+        htmlFileUrl: string;
+        httpServerUrl: string;
+        name: string;
+        version: string;
+    };
+    private updateHtmlFileUrl;
+}
+export default NativeApplication;
